42d420dda807e8715a758549b58dcf6fdb167a8d
CarlosRM-25 <158112977+CarlosRM-25@users.noreply.github.com>
2025-12-13 20:27:43 -0600

    Add files via upload

---
+++ Models/InventarioInteligenteDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Inventario_Inteligente.Models
+{
+    public class InventarioInteligenteDbContext
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(200)]
+        public string Name { get; set; }
+
+        [Range(0, double.MaxValue)]
+        public decimal Price { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int Stock { get; set; }
+    }
+}
---
+++ Models/InventoryStatisticsViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Inventario_Inteligente.Models
+{
+    public class InventoryStatisticsViewModel
+    {
+        public List<Controllers.Product> ProductsOrderedByPriceDesc { get; set; }
+        public decimal AveragePrice { get; set; }
+        public decimal TotalInventoryValue { get; set; }
+        public List<Controllers.Product> CriticalStockProducts { get; set; }
+    }
+}
