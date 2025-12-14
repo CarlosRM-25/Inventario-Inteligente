ca5338e289a85f8accc5f0ef08ce3fa9d2afb532
CarlosRM-25 <158112977+CarlosRM-25@users.noreply.github.com>
2025-12-13 20:27:09 -0600

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Inventario_Inteligente.Data
+{
+    public class ApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        internal async Task SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Update(object productToUpdate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+}
